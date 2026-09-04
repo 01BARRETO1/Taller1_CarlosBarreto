f78cbef2298c7df621771012b5490cd07bfb3b52
01BARRETO1 <karbar_1990@hotmail.com>
2026-09-04 04:05:33 -0500

    Control de Salto y Colisión por Tags

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour{
     public float moveSpeed=5f; //Velocidad de movimiento
     public float jumpForce=7f;//Fuerza de salto
     private Rigidbody2D rd;//Referencia al componente
-    private bool isGround;//Variable booleana para verificar si está en el suelo
+    private bool isGround;//Variable booleana para verificar si está en el piso
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
         rd=GetComponent<Rigidbody2D>();//localitation
@@ -16,6 +16,30 @@ public class PlayerController : MonoBehaviour{
     void Update(){
         float move = Input.GetAxis("Horizontal");//localitation
         rd.linearVelocity=new Vector2(move * moveSpeed, rd.linearVelocityY);
+
+        //permitir el salto
+        if(Input.GetButtonDown("Jump") && isGround){
+            rd.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+            
+        }
+        
+    }
+
+    //evento OnCollisionEnter2D, colisiona con la plataforma.
+    void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Piso"))
+        {
+             isGround = true;
+        }
+       
+    }
+
+    //Implementar OnCollisionExit2D
+    void OnCollisionExit2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Piso"))
+        {
+            isGround = false;
+        }
         
     }
 }
