c724f4f9b83cfaa37e10903ce08b9200f45aeff8
01BARRETO1 <karbar_1990@hotmail.com>
2026-09-04 02:06:54 -0500

    Creación del Script de C#

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+    public float moveSpeed=5f; //Velocidad de movimiento
+    public float jumpForce=7f;//Fuerza de salto
+    private Rigidbody2D rd;//Referencia al componente
+    private bool isGround;//Variable booleana para verificar si está en el suelo
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
