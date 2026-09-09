ed97b06dc6c56bec5d789cbe7cdb8d743488052c
01BARRETO1 <karbar_1990@hotmail.com>
2026-09-09 02:14:12 -0500

    Sistema de Seguimiento de Cámara (C#)

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   public Transform target;
+   public float smoothSpeed = 5f;
+   public Vector3 offset = new Vector3(0, 2,-10);
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.position+offset;
+
+            Vector3 smoothePosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+
+            transform.position = smoothePosition;
+        }
+    }
+}
